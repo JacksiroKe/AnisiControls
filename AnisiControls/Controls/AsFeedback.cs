79a902fd5e54626bd0d4aa3698db0abf77ce0dac
Jacksiro <jaksiro@gmail.com>
2019-05-31 12:48:10 +0300

    Add files via upload

---
+++ AnisiControls/Controls/AsFeedback.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AnisiControls
+{
+    public class AsFeedback : Control
+    {
+        public PictureBox act = new PictureBox();
+        public Label msg = new Label();
+        public AsButton clr = new AsButton();
+        public Timer timer = new Timer();
+
+        private float interval;
+        private bool isPositive, isTimed;
+        Color background;
+        int intx = 35, inty = 0;
+
+        public AsFeedback()
+        {
+            background = Color.LightGreen;
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.UserPaint, true);
+            
+            this.act.Parent = this;
+            this.msg.Parent = this;
+            this.clr.Parent = this;
+            this.Controls.Add(this.act);
+            this.Controls.Add(this.msg);
+            this.Controls.Add(this.clr);
+
+            this.act.Size = new Size(22, 22);
+            this.act.BackColor = Color.Transparent;
+            this.act.Image = AnisiControls.Properties.Resources.info;
+            this.act.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+
+            this.msg.AutoSize = true;
+            this.msg.BorderStyle = BorderStyle.None;
+            this.msg.Font = this.Font;
+            this.msg.ForeColor = Color.Black;
+            this.msg.Size = new Size(35, 13);
+            this.msg.TabIndex = 1;
+            this.msg.Text = this.Text;
+
+            this.clr.Text = "X";
+            this.clr.Radius = 12;
+            this.clr.AutoSize = true;
+            this.clr.Font = new Font("Arial Rounded MT", 10, FontStyle.Bold);
+            this.clr.Size = new Size(24, 24);
+            this.clr.Active1 = Color.White;
+            this.clr.Active2 = Color.Gray;
+            this.clr.Inactive1 = Color.White;
+            this.clr.Inactive2 = Color.LightGray;
+
+            this.timer.Enabled = this.Visible;
+            this.Text = null;
+            this.Font = new Font("Trebuchet MS", 12);
+            this.ForeColor = Color.Black;
+            this.Size = new Size(300, 50);
+
+            DoubleBuffered = true;
+            IsPositive = true;
+            IsTimed = false;
+
+            this.timer.Interval = 2500;
+            this.timer.Tick += timer_Tick;
+
+            this.msg.TextChanged += msg_TextChanged;
+            this.clr.Click += clr_onClick;
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            this.timer.Enabled = false;
+            this.Visible = false;
+            Invalidate();
+        }
+        protected override Size DefaultSize
+        {
+            get
+            {
+                return new Size(400, 40);
+            }
+        }
+
+        public bool IsPositive
+        {
+            get
+            {
+                return isPositive;
+            }
+            set
+            {
+                isPositive = value;
+                if (isPositive)
+                {
+                    this.BackColor = Color.LightGreen;
+                    background = Color.LightGreen;
+                }
+                else
+                {
+                    this.BackColor = Color.LightSalmon;
+                    background = Color.LightSalmon;
+                }
+                Invalidate();
+            }
+        }
+        public bool IsTimed
+        {
+            get
+            {
+                return isTimed;
+            }
+            set
+            {
+                isTimed = value;
+                if (isTimed) timer.Enabled = true;
+                else this.timer.Enabled = false;
+                Invalidate();
+            }
+        }
+
+        public float Interval
+        {
+            get
+            {
+                return this.timer.Interval;
+            }
+            set
+            {
+                interval = value;
+                Invalidate();
+            }
+        }
+        private void clr_onClick(object sender, EventArgs e)
+        {
+            this.timer.Enabled = false;
+            this.Visible = false;
+        }
+
+
+        void msg_TextChanged(object sender, EventArgs e)
+        {
+            this.Text = msg.Text;
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            this.msg.Text = this.Text;
+        }
+        protected override void OnFontChanged(EventArgs e)
+        {
+            base.OnFontChanged(e);
+            this.msg.Font = this.Font;
+            Invalidate();
+        }
+        protected override void OnForeColorChanged(EventArgs e)
+        {
+            base.OnForeColorChanged(e);
+            this.msg.ForeColor = this.ForeColor;
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            inty = (Height - this.msg.Font.Height) / 2;
+
+            this.act.Location = new Point(5, Height / 4);
+            this.msg.Location = new Point(intx, inty - 1);
+            this.msg.Width = Width - (intx + 40);
+            this.clr.Location = new Point(Width - 30, Height / 4);
+
+            e.Graphics.DrawRectangle(Pens.Black, 0, 0, Width - 1, Height - 1);
+            using (SolidBrush brush = new SolidBrush(background))
+                e.Graphics.FillRectangle(brush, 1, 1, Width - 2, Height - 2);
+            base.OnPaint(e);
+        }
+       
+    }
+}
---
+++ AnisiControls/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Controls by Jack Siro")]
+[assembly: AssemblyDescription("Button, Switch, Feedback, Sliding Label, Circular Progress Bar, Spinning Circles and so much custom controls")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jack Siro")]
+[assembly: AssemblyProduct("AnisiControls")]
+[assembly: AssemblyCopyright("Copyright © Jacksiro 2018")]
+[assembly: AssemblyTrademark("Jacksiro")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8622700a-4031-45d0-9c4f-5d415b97972d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.5.0")]
+[assembly: AssemblyFileVersion("1.2.5.0")]
